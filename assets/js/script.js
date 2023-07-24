0ac90f0288af308d2a4726808c4f30c8c0fb2342
Mona Lisa <waynecharrette@gmail.com>
2023-07-24 16:03:06 -0400

    inital commit

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+var timeEl = document.querySelector(".time");
+
+var secondsLeft = 75; 
+
+function setTime() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = "Time: " + secondsLeft;
+
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+      sendMessage();
+    }
+
+  }, 1000);
+}
+
+// Function to create and append colorsplosion image
+function sendMessage() {
+  timeEl.textContent = " ";
+  var imgEl = document.createElement("img");
+  imgEl.setAttribute("src", "Time's up.");
+  mainEl.appendChild(imgEl);
+
+}
+
+setTime();
