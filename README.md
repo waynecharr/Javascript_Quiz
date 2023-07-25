63b18a9ed1d315ba7cb8ae76747e680975dd8237
Mona Lisa <waynecharrette@gmail.com>
2023-07-25 15:05:06 -0400

    updated index and customized array, removed timer

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 - "initial commit" contains basic index, css, and started javascript
 - "updated css added classes for buttons" - haven't worked out much of the javascript beyond the timer. updated much of the stylesheets.
+- "updated index and customized array, removed timer" - created an array for the javascript for the question prompts. I have what I believe I need for the index.html. I now have a better understanding of how display:none display: block function so that part is figured out. 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,32 @@
-var timeEl = document.querySelector(".time");
 
-var secondsLeft = 75; 
-
-function setTime() {
-  // Sets interval in variable
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeEl.textContent = "Time: " + secondsLeft;
-
-    if(secondsLeft === 0) {
-      // Stops execution of action at set interval
-      clearInterval(timerInterval);
-      // Calls function to create and append image
-      sendMessage();
-    }
-
-  }, 1000);
-}
-
-// Function to create and append colorsplosion image
-function sendMessage() {
-  timeEl.textContent = " ";
-  var imgEl = document.createElement("img");
-  imgEl.setAttribute("src", "Time's up.");
-  mainEl.appendChild(imgEl);
-
-}
-
-setTime();
+// This array sets the questions that will be used in the quiz. 
+var prompt = [
+  {
+    // question - is the question being asked.
+    question: "Commonly used data types DO Not include:",
+    // choices - is the list of choices available. These are buttons, and they start as 0, 1, 2, 3.
+    choices: ["1. Strings", "2. Booleans", "3. Alerts", "4. Numbers"],
+    // answer - is the answer, pretty straight forward.
+    answer: "3. Alerts"
+  },
+  {
+    question: "The condition in an if/else statement is enclosed with ______.",
+    choices: ["1. Quotes", "2. Curly brackets", "3. Parenthesis", "4. Square brackets"],
+    answer: "3. Parenthesis"
+  },
+  {
+    question: "Arrays in Javascript can be used to store ______.",
+    choices: ["1. Numbers and strings", "2. Other arrays", "3. Booleans", "4. All of the above"],
+    answer: "4. All of the above"
+  },
+  {
+    question: "String values must be enclosed within ______ when being assigned to variables.",
+    choices: ["1. Commas", "2. Curly brackets", "3. Quotes", "4. Parenthesis"],
+    answer: "3. Quotes"
+  },
+  {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["1. JavaScript", "2. Terminal/bash", "3. For loops", "4. Console.log"],
+    answer: "4. Console.log"
+  }
+];
